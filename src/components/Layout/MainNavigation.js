be28107dddd6fc96859633c1021c22a4f719e93e
MayankTiwari9 <mt1027319@gmail.com>
2023-11-29 22:53:42 +0530

    Logout Done

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import { useContext } from "react";
